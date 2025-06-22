af6f36204e761eaacd9527a1985c753f984af3f3
MarkelRoman05 <markromagonz@gmail.com>
2025-06-22 23:42:59 +0200

    Añade nueva estructura de proyectos y mejora la presentación de la página de error 404

---
+++ src/data/projects.ts
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    slug: "sistema-de-proteccion-y-analisis-de-ciberinteligencia",
+    title: "Sistema de Protección y Análisis de Ciberinteligencia",
+    description:
+      "El proyecto tiene como objetivo desarrollar una plataforma avanzada de ciberinteligencia que permita gestionar, analizar y compartir información sobre amenazas de manera estructurada y automatizada. Esta plataforma permite realizar el modelado de la “Postura de Ciberseguridad” (con puntuaciones) de una organización a partir de la identificación de las amenazas a las que hace frente. Para ello, utilizaremos el marco de MITRE ATT&CK. ATT&CK es una guía para clasificar y describir ciberataques e intrusiones. En lugar de observar los resultados de un ataque, identifica tácticas que indican que un ataque está en progreso.",
+    image: "/images/panacea.jpg",
+    tags: ["Vue.js", "Tailwind CSS", "FastAPI", "ArangoDB", "Docker"],
+    codeUrl: "https://github.com/Markel05-Elorrieta/DAM2_E2_T6_DI",
+    liveUrl: "",
+  },
+  {
+    slug: "dashboard-gestion-alumnos",
+    title: "Dashboard de gestión de alumnos y profesores",
+    description:
+      "Una aplicación web que permite gestionar alumnos y profesores de un centro educativo. Incluye funcionalidades para el registro, edición y eliminación de usuarios, así como la asignación de alumnos a clases y profesores a materias. Permite visualizar listados, filtrar y buscar información de manera eficiente. Además, cuenta con autenticación de usuarios y diferentes roles de acceso para garantizar la seguridad y privacidad de los datos.",
+    image:
+      "https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2020/09/hipertextual-principales-plataformas-dar-clase-internet-2020079983.jpg?fit=1920%2C1372&quality=50&strip=all&ssl=1",
+    tags: ["Angular", "Tailwind CSS", "MySQL", "Express", "Node.js"],
+    codeUrl: "https://github.com/Markel05-Elorrieta/DAM2_E2_T6_DI",
+    liveUrl: "",
+  },
+  {
+    slug: "talk2sign",
+    title: "Talk2Sign · Traductor de texto/voz a lenguaje de signos",
+    description:
+      "Una aplicación móvil innovadora que traduce texto y voz a lenguaje de signos, representando el resultado mediante un avatar 3D animado. El objetivo principal es facilitar la comunicación entre personas oyentes y personas sordas o con discapacidad auditiva, eliminando barreras de accesibilidad. La app permite introducir texto manualmente o mediante reconocimiento de voz, y genera la interpretación en lenguaje de signos de manera visual e interactiva. Además, cuenta con una interfaz intuitiva y recursos educativos para aprender vocabulario en lengua de signos.",
+    image:
+      "https://img.freepik.com/vector-gratis/diseno-hombres-negocios-hablando_1133-349.jpg",
+    tags: ["Android Studio", "Java", "AWS", "Figma"],
+    codeUrl: "https://github.com/Markel05-Elorrieta/Talk2Sign_APP",
+    liveUrl: "",
+  },
+];
